76e85bd95125bb9113f9243f0cf4711b4cb770d8
Sanskar318 <137708973+Sanskar318@users.noreply.github.com>
2026-01-17 15:41:21 +0530

    Update stream.php

--- api/stream.php
+++ api/stream.php
@@ -1,14 +1,6 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/vnd.apple.mpegurl");
-
-$id = $_GET['id'] ?? '';
-
-if (!empty($id)) {
-    // Note: Asali FanCode resolver logic future mein yahan lagega
-    // Filhaal test m3u8 stream par redirect kar raha hai
-    $stream_url = "https://dai.google.com/linear/hls/event/sid/master.m3u8";
-    header("Location: $stream_url");
-    exit;
-}
+header("Location: https://dai.google.com/linear/hls/event/sid/master.m3u8");
+exit;
 ?>
