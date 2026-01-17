1158dcd479fb4bd12f618d8b7318c87e086fe3b8
Sanskar318 <137708973+Sanskar318@users.noreply.github.com>
2026-01-17 15:00:31 +0530

    Update stream.php

--- api/stream.php
+++ api/stream.php
@@ -5,10 +5,10 @@ header("Content-Type: application/vnd.apple.mpegurl");
 $id = $_GET['id'] ?? '';
 
 if (!empty($id)) {
-    // FanCode resolver logic
-    // Filhaal hum test m3u8 par bhej rahe hain, asali matches ke liye resolver replace hoga
-    $stream_link = "https://dai.google.com/linear/hls/event/sid/master.m3u8";
-    header("Location: $stream_link");
+    // Note: Asali FanCode resolver logic future mein yahan lagega
+    // Filhaal test m3u8 stream par redirect kar raha hai
+    $stream_url = "https://dai.google.com/linear/hls/event/sid/master.m3u8";
+    header("Location: $stream_url");
     exit;
 }
 ?>
