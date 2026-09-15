51d7fc631218eafc207a072e3e36ca2d260f9c60
Sanskar318 <137708973+Sanskar318@users.noreply.github.com>
2026-09-16 00:43:00 +0530

    Delete api directory

--- api/stream.php
+++
@@ -1,6 +0,0 @@
-<?php
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/vnd.apple.mpegurl");
-header("Location: https://dai.google.com/linear/hls/event/sid/master.m3u8");
-exit;
-?>
